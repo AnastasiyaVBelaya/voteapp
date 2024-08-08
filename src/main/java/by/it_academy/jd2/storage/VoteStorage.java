9476d996c83443e80a8e3799b04aae2efe58f18e
Anastasiya Belaya <anastasia-belaya@mail.ru>
2024-08-08 22:04:26 +0300

    добавление отдельного слоя хранилища

---
+++ src/main/java/by/it_academy/jd2/storage/VoteStorage.java
@@ -0,0 +1,39 @@
+package by.it_academy.jd2.storage;
+
+import by.it_academy.jd2.dto.VoteDTO;
+import by.it_academy.jd2.storage.api.IVoteStorage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VoteStorage implements IVoteStorage {
+
+    private final static VoteStorage instance = new VoteStorage();
+
+    private Map<String, Integer> artist = new HashMap<>();
+    private Map<String, Integer> genre = new HashMap<>();
+    private List<String> abouts = new ArrayList<>();
+
+    private VoteStorage() {
+    }
+
+    @Override
+    public void create(VoteDTO vote) {
+        createArtistOrGenre(artist, vote.getArtist());
+
+        for (String genre : vote.getGenre()) {
+            createArtistOrGenre(this.genre, genre);
+        }
+        abouts.add(vote.getAbout());
+    }
+
+    private void createArtistOrGenre(Map<String, Integer> data, String key){
+        data.compute(key, (k, v) -> (v == null) ? 1 : v + 1);
+    }
+
+    public static VoteStorage getInstance() {
+        return instance;
+    }
+}
